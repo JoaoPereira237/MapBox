9c98ad9d8ff4d5631563ed39f6fd60b927a07541
João Pedro Salgado Pereira <8220102@estg.ipp.pt>
2024-09-24 13:33:51 +0100

    gfgg

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import './App.css'
+
+function App() {
+
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiODIyMDgxNiIsImEiOiJjbTFnZG5yZm0wMnUwMmxzaXltbGpkejQzIn0.-z9XH3aUAFvj6xR-qG-0sg'
+    
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: [-8.0, 39.5],
+      zoom: 6.12,
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
+
+  return (
+    <>
+      <div id='map-container' ref={mapContainerRef}/>
+    </>
+  )
+}
+
+export default App
